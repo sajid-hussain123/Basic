0f2615582e530bd00e757006581f730ac89fe0ac
Admin <Admin@DESKTOP-2MGVNL3.bbroute>
2023-04-05 17:48:13 +0530

    Reverse String

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        String str = "Sajid";
+        for(int i = str.length()-1;i>=0;i--){
+            System.out.print(str.charAt(i));
+        }
+    }
+
+    }
+
