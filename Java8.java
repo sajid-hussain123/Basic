7070c16b29e4b89f83ce1f817c4ab93474779948
Admin <Admin@DESKTOP-2MGVNL3.bbroute>
2023-05-01 12:46:35 +0530

    PrintingDupliactesElements

--- Java8.java
+++ Java8.java
@@ -1,22 +1,32 @@
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import static java.util.Arrays.*;
 
 public class Java8 {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 1, 4, 5, 5);
-        System.out.println(list);
+       /* System.out.println(list);
         Collections.sort(list);
         System.out.println(list);
         Collections.sort(list,(i1,i2)->i1>i2?-1:i1<i2?+1:0);
+        System.out.println(list);*/
         System.out.println(list);
-        Set<Integer> set = new HashSet<>(list);
-        System.out.println(set);
+        Set<Integer> items = new HashSet<>();
 
-        Map<Integer, Long> map = list.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
-        System.out.println(map);
+        list.stream()
+                .filter(n -> !items.add(n)) // Set.add() returns false if the element was already in the set.
+                .collect(Collectors.toSet()).forEach(System.out::println);
+
+        System.out.println(items);
+
+
+
+
+
+        /*Map<Integer, Long> map = list.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+        System.out.println(map);*/
 
     }
 }
+
+
+
